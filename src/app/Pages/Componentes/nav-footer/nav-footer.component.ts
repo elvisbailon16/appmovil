90bfcaa6af22566bd0a3653d786010e051373b7d
ElvisBailon <elvisbailon16@gmail.com>
2026-06-19 14:03:17 -0500

    meno footer

--- src/app/Pages/Componentes/nav-footer/nav-footer.component.ts
+++ src/app/Pages/Componentes/nav-footer/nav-footer.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonToolbar, IonIcon, IonTitle, IonButton } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-nav-footer',
   templateUrl: './nav-footer.component.html',
   styleUrls: ['./nav-footer.component.scss'],
+  imports: [IonToolbar, IonIcon, IonTitle, IonButton]
 })
 export class NavFooterComponent  implements OnInit {
 
